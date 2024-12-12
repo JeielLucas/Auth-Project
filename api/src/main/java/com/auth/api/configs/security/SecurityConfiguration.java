bfa1ac7db9f8f3f34f9f8ec9db2baaa55e65cb3b
Jeiel <jeiellucas0404@gmail.com>
2024-12-12 18:21:16 -0300

    feat: Removendo rotas desnecessarias

--- api/src/main/java/com/auth/api/configs/security/SecurityConfiguration.java
+++ api/src/main/java/com/auth/api/configs/security/SecurityConfiguration.java
@@ -35,11 +35,7 @@ public class SecurityConfiguration {
     };
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED_GET = {
             "/api/v2/auth/ping",
-            "/api/v2/test/users",
             "/api/v2/auth/ativar-conta"
-    };
-    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED_DELETE = {
-            "/api/v2/auth/delete-users"
     };
 
     @Bean
@@ -51,9 +47,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(HttpMethod.POST, ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED_POST).permitAll()
                         .requestMatchers(HttpMethod.GET, ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED_GET).permitAll()
-                        .requestMatchers(HttpMethod.DELETE, ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED_DELETE).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v2/auth/validar-token").authenticated()
-                        .anyRequest().permitAll()
+                        .anyRequest().denyAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
