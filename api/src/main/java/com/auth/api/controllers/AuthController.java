d0f0c24ab2616f9f0c8166b2159899b91b704a4a
Jeiel <jeiellucas0404@gmail.com>
2024-12-20 14:56:06 -0300

    refactor: Services impementando uma Interface de Services

--- api/src/main/java/com/auth/api/controllers/AuthController.java
+++ api/src/main/java/com/auth/api/controllers/AuthController.java
@@ -4,7 +4,7 @@ import com.auth.api.dtos.ApiResponse;
 import com.auth.api.dtos.LoginRequestDTO;
 import com.auth.api.dtos.RegisterRequestDTO;
 import com.auth.api.dtos.ResetPasswordRequest;
-import com.auth.api.services.AuthService;
+import com.auth.api.services.AuthServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -16,40 +16,45 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v2/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceImpl authServiceImpl;
 
-    public AuthController(final AuthService authService) {
-        this.authService = authService;
+    public AuthController(final AuthServiceImpl authServiceImpl) {
+        this.authServiceImpl = authServiceImpl;
     }
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse> Register(@Valid @RequestBody RegisterRequestDTO user){
-        return authService.RegisterUser(user);
+        return authServiceImpl.register(user);
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> Login(@Valid @RequestBody LoginRequestDTO user, HttpServletResponse response){
-        return authService.LoginUser(user, response);
+        return authServiceImpl.login(user, response);
+    }
+
+    @PostMapping("/e")
+    public ResponseEntity<ApiResponse> googleLogin(){
+        return authServiceImpl.googleLogin("1");
     }
 
     @PostMapping("/reset-password")
     public ResponseEntity requestPasswordReset(@RequestParam String email){
-        return authService.requestPasswordReset(email);
+        return authServiceImpl.requestPasswordReset(email);
     }
 
     @PostMapping("/redefinir-senha")
     public ResponseEntity resetPassword(@RequestParam String token, @RequestBody ResetPasswordRequest password){
-        return authService.resetPassword(token, password);
+        return authServiceImpl.resetPassword(token, password);
     }
 
     @GetMapping("/ativar-conta")
     public ResponseEntity activateUser(@RequestParam String token, HttpServletResponse response){
-        return authService.activateUser(token, response);
+        return authServiceImpl.activateUser(token, response);
     }
 
     @GetMapping("/validar-token")
     public ResponseEntity validarToken(HttpServletRequest request){
-        return authService.validarToken(request);
+        return authServiceImpl.validarToken(request);
     }
 
     @GetMapping("/ping")
--- api/src/main/java/com/auth/api/services/EmailService.java
+++ api/src/main/java/com/auth/api/services/EmailService.java
@@ -1,73 +1,11 @@
 package com.auth.api.services;
 
-import com.auth.api.entities.Email;
 import com.auth.api.entities.User;
-import com.auth.api.enums.StatusEmail;
-import com.auth.api.repositories.EmailRepository;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
-@Service
-public class EmailService {
+public interface EmailService {
 
 
-    private final EmailRepository emailRepository;
-    private final JavaMailSender mailSender;
+    void sendActivationEmail(User user, String token);
 
-    public EmailService(EmailRepository emailRepository, JavaMailSender mailSender) {
-        this.emailRepository = emailRepository;
-        this.mailSender = mailSender;
-    }
-
-    @Async
-    public void sendActivationEmail(User user, String token) {
-        Email email = new Email();
-        email.setOwnerRef("api-auth-project");
-        email.setEmailTo(user.getEmail());
-        email.setSubject("Ativação de conta - Auth Project");
-        email.setText(
-                "Clique no link abaixo para ativar sua conta \n" +
-                "http://localhost:5173/ativar-conta/" + token
-        );
-        email.setCreatedAt(LocalDateTime.now());
-
-        sendEmail(email);
-    }
-
-    @Async
-    public void sendResetPasswordEmail(User user, String token) {
-        Email email = new Email();
-        email.setOwnerRef("api-auth-project");
-        email.setEmailTo(user.getEmail());
-        email.setSubject("Redefinição de senha - Auth Project");
-        email.setText(
-                "Clique no link abaixo para redefinir sua senha \n"
-                + "http://localhost:5173/redefinir-senha/" + token
-        );
-        email.setCreatedAt(LocalDateTime.now());
-
-        sendEmail(email);
-    }
-
-    private void sendEmail(Email email){
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        try{
-            message.setTo(email.getEmailTo());
-            message.setSubject(email.getSubject());
-            message.setText(email.getText());
-
-            mailSender.send(message);
-            email.setStatusEmail(StatusEmail.SENT);
-        }catch (MailException e){
-            email.setStatusEmail(StatusEmail.ERROR);
-        }finally {
-            emailRepository.save(email);
-        }
-    }
+    void sendResetPasswordEmail(User user, String token);
 }
